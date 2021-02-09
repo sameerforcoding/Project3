aa3d29210003aa89176e87f4e2c82f23ec2eee94
sameerforcoding <78414003+sameerforcoding@users.noreply.github.com>
2021-02-09 12:27:42 +0530

    Add files via upload

---
+++ Project 3/sketch.js
@@ -0,0 +1,21 @@
+function draw() {
+    draw();
+
+    //level two
+    block8 = new block(330, 235, 30, 40);
+    block9 = new block(360, 235, 30, 40);
+    block10 = new block(390, 235, 30, 40);
+    block11 = new block(420, 235, 30, 40);
+    block12 = new block(450, 235, 30, 40);
+    //level three
+    block13 = new block(360, 195, 30, 40);
+    block14 = new block(390, 195, 30, 40);
+    block15 = new block(420, 195, 30, 40);
+    //top
+    block16 = new block(block390, 155, 30, 40);
+
+
+
+
+
+}
